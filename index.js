ff9db97dd7aaaf956494c4f8815ddab55c93d244
Niko Eklöf <80674961+nikoeklof@users.noreply.github.com>
2021-08-24 10:35:58 +0300

    rev 0.8
    lisätty ohjeet, pisteet

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ function step() {
     player.moveSnake();
     isGameOver();
     checkAppleCollision();
+
+
     drawApple();
     player.drawPlayer();
 
@@ -52,6 +54,11 @@ function step() {
 function drawApple() {
     x = appleX
     y = appleY
+        //tarkistetaan ettei omena ole pelaajan sisällä
+    if (x == player.x && y == player.y) {
+        x = appleX
+        y = appleY
+    }
     gameboard_ctx.fillStyle = 'red'
     gameboard_ctx.strokeStyle = 'lightgreen'
     gameboard_ctx.lineWidth = 2
@@ -64,16 +71,23 @@ function checkAppleCollision() {
     if (appleX == player.x && appleY == player.y) {
         appleX = Math.floor(Math.random() * tilecount) * 20
         appleY = Math.floor(Math.random() * tilecount) * 20
+        newApplex = appleX
+        newAppley = appleY
+            //tarkistetaan ettei uusi omena ole käärmeen sisällä, jos on niin luodaan uusi omena
+        for (let i = 0; i < player.snake_parts.length; i++) {
+            if (newApplex == player.snake_parts[i].x && newAppley == player.snake_parts[i].y) {
+                appleX = Math.floor(Math.random() * tilecount) * 20
+                appleY = Math.floor(Math.random() * tilecount) * 20
+            }
+        }
         player.addpart()
         overallscore++
-        player.score++
-            if (player.score === 1) {
-                clearInterval(gameLoop)
-                gameLoop = setInterval(step, 1000 / gamespeed)
-                gamespeed = gamespeed + 0.1
-                player.score = 0
-            }
+        gamespeed = gamespeed + 0.25
+        clearInterval(gameLoop)
+        gameLoop = setInterval(step, 1000 / gamespeed)
         console.log(gamespeed)
+        document.getElementById('score').innerHTML = overallscore
+
 
     }
 
@@ -154,5 +168,69 @@ function change_direction(event) {
         }
     } else {
         return
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Tästä alkaa pelaaja luokka, oli erillisessä tiedostossa mutta ei koulun palvelimella toiminut jostain syystä
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const playercolor = "green"
+const playerbordercolor = ""
+
+function Player(x, y) {
+    class PlayerParts {
+        constructor(x, y) {
+            this.x = x
+            this.y = y
+        }
+    }
+
+    this.x = x
+    this.y = y
+    this.width = 20
+    this.height = 20
+    this.playercolor = playercolor
+    this.playerbordercolor = playerbordercolor
+    this.snake_parts = []
+    this.playerLength = 3;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.score = 0;
+
+
+
+    this.drawplayer = function() {
+
+        gameboard_ctx.fillStyle = 'blue'
+        gameboard_ctx.strokeStyle = 'white'
+        gameboard_ctx.strokeWidth = 1
+        for (let i = 0; i < this.snake_parts.length; i++) {
+            let part = this.snake_parts[i]
+            gameboard_ctx.fillRect(part.x, part.y, 20, 20)
+            gameboard_ctx.strokeRect(part.x, part.y, 20, 20)
+
+        }
+        //lisää uuden osan
+        this.snake_parts.push(new PlayerParts(this.x, this.y))
+        if (this.snake_parts.length > this.playerLength) {
+            //poistaa vanhimman osan
+            this.snake_parts.shift();
+        }
+        //pelaajan väri
+        gameboard_ctx.fillStyle = this.playercolor
+        gameboard_ctx.fillRect(this.x, this.y, this.width, this.height)
+        gameboard_ctx.strokeRect(this.x, this.y, 20, 20)
+    }
+    this.addpart = function() {
+        this.playerLength++
+    }
+
+    this.drawPlayer = function() {
+
+        this.drawplayer()
+
+    }
+    this.moveSnake = function() {
+        this.x = this.x + this.velocityX
+        this.y = this.y + this.velocityY
     }
 }
